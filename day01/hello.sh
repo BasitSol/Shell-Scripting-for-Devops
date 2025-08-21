1314cd91b464df9c623305d79b008166a6113a10
BasitSol <silence_speed@Basit-PC.localdomain>
2025-08-22 03:57:31 +0500

    Add Day01 hello.sh script

---
+++ day01/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello world this is my first script for the Shell scripting"
+echo "This is the first ever program of the shell command in this game"
+echo "And ofcourse i am very excited about that to actually do that"
+
