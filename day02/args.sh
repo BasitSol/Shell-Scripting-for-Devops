a996a055443b79991a540ac2282cfd885394cfc6
BasitSol <silence_speed@Basit-PC.localdomain>
2025-08-27 03:52:34 +0500

    Updated scripts in the day02

---
+++ day02/args.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This is the king ki dunya
+
+<< comment
+
+Evrything thing written over there is being ignored 
+comment
+
+name="Ali"
+echo "The name of the student is $name "
+
+echo "Enter the name of the Professor:"
+
+read username
+
+echo "The name is $username"
+
+echo "The first arg is $0 and Second is  $1"
+
---
+++ day02/check-loyal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<< disclaimer
+This is just for the infortanment purpose
+disclaimer
+
+read -p "Enter the name of the wife:" bandi
+read -p "Enter Aslam Love %:" pyaar
+
+if [[ $bandi == "alina Bhabi" ]];then 
+	echo "Aslam is the loyal person"
+elif [[ $pyaar -ge 100 ]];then
+	echo "Aslam is loyal"
+else
+	echo "Aslam is a cheater"
+
+fi
+
